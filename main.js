d10d3e5dfcfaae4789fc624c2c5b07a45f2b3fd3
Nishant-code-star <79645199+Nishant-code-star@users.noreply.github.com>
2021-09-03 10:03:47 -0400

    Update main.js

--- main.js
+++ main.js
@@ -44,8 +44,8 @@ else{
     console.log(results);
     document.getElementById("result_emotion_name").innerHTML=results[0].label;
     document.getElementById("result_emotion_name2").innerHTML=results[1].label;
-    prediction_1=result[0].label;
-    prediction_2=result[1].label;
+    prediction_1=results[0].label;
+    prediction_2=results[1].label;
     speak();
     if(results[0].label=="Happy"){
         document.getElementById("update_emoji").innerHTML="&#128522"
